57176ab39ce22d1be7360fbede56198504b19090
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-03-09 03:53:02 -0800

    update

--- model/Task.php
+++ model/Task.php
@@ -2,11 +2,35 @@
 
 class TaskException extends Exception {}
 
-class Task{
+class Task{ 
 
     private $_id;
     private $_title;
     private $_description;
     private $_deadline;
     private $_completed;
+
+    //getters and setters
+
+    public function getID(){
+        return $this->_id;
+    }
+
+    public function getTitile(){
+        return $this->_title;
+    }
+
+    public function getDescription(){
+        return $this->_description;
+    }
+
+    public function getDeadline(){
+        return $this->_deadline;
+    }
+
+    public function getCompleted(){
+        return $this->_completed;
+    }
+
+
 }
