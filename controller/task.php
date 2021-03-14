3af26d2dd76c8b832d140043da97b1fa1e6feb42
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-03-14 16:31:23 -0700

    update

--- controller/task.php
+++ controller/task.php
@@ -3,3 +3,11 @@
 require_once('db.php');
 require_once('../model/Task.php');
 require_once('../model/Response.php');
+
+try{
+    $writeDB = DB::connectWriteDB();
+    $readDB = DB::connectReadDB();
+
+} catch(PDOException $ex){
+    kk
+}
