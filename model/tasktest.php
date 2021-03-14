af6e7481c9276b1e9836c9224c06e1fec2416862
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-03-13 16:57:43 -0800

    update

--- model/tasktest.php
+++ model/tasktest.php
@@ -6,7 +6,9 @@ try{
     $task = new Task(1, "Title here", "Description here", "01/01/2019 12:00", "N");
     header('Content-type: application/json;charset=UTF-8');
     echo json_encode($task->returnTaskAsArray());
-    
+
 } catch (TaskException $ex) {
     echo "Error: ".$ex->getMessage();
 }
+
+// File not needed. Just for testing task.php model
